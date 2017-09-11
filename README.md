e044f5eea4db7c1335600df214ae3187ca398c2b
ortegadiana <dortega@xebia.fr>
2017-09-11 18:46:44 +0200

    readme

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-Imaginons un supermarché.
+# Supermarket Price
 
-Le supermarché a une liste de produits chaque un avec son price et quantité en stock.
-
-Un acheteur arrive au supermarché et achete quelques produits en stock.
-
-creer un petit programme qui permette au supermarche de generer le total acheté et de retirer du stock.
-
-But: decouvrir types de donées, operations basiques et notation
+you can change to step-1, step-2 or step-3 branches to start the exercise 
--- supermarketPrice.go
+++ supermarketPrice.go
@@ -1,13 +1,12 @@
 package main
 
 import "fmt"
-import "supermarketPrice/super"
 
-//you can init an array for example: b := [2]string{"Penn", "Teller"} or b := [...]string{"Penn", "Teller"}
-//or you can init an slice, for example: letters := []string{"a", "b", "c", "d"} or s = make([]byte, 5, 5)
 func main() {
-	fmt.Println("--- Supermarket Price ---")
-	//productSelection := super.ProductSelection{Amt: 10, Prod: nil}
-	//stock := super.Stock{contents: []super.ProductSelection{productSelection}}
-	//stock.sellProduct(productSelection)
+	var x int // Variable declaration. Variables must be declared before use.
+	x = 3     // Variable assignment.
+	// "Short" declarations use := to infer the type, declare, and assign.
+	y := 4
+
+	fmt.Println(x,y)
 }
